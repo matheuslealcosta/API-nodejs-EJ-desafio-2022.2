8e77f3b220d7006f2d291ef16fbbe0aa6b70ef93
“MatGod135” <matheuslealcosta@icloud.com>
2022-12-06 18:10:08 -0300

    cria função deletar e excluir membros

--- src/controler/membrosController.js
+++ src/controler/membrosController.js
@@ -18,27 +18,49 @@ class membrosController {
             .populate('departamentos')
             .populate('cargos')
             .exec((req, res) => {
-            if (err) {
-                res.status(400).send({ messege: `${err.messsage} - Id incorreto` });
-            }else{
-                res.status(200).send(membros);
-            }
-        })
+                if (err) {
+                    res.status(400).send({ messege: `${err.messsage} - Id incorreto` });
+                } else {
+                    res.status(200).send(membros);
+                }
+            })
     }
 
-    static cadastrarMembro =  (req, res) => {
+    static cadastrarMembro = (req, res) => {
         let membro = new membros.$where(req.body);
 
         membro.save((err) => {
-            if(err){
-                res.status(500).send({message: `${err.message} - falha ao cadastrar novo membro`});
-            }else{
+            if (err) {
+                res.status(500).send({ message: `${err.message} - falha ao cadastrar novo membro` });
+            } else {
                 res.status(201).send(livro.toJSON());
             }
         })
     }
 
-    
+    static atulizarMembro = (req, res) => {
+        const { id } = req.params;
+
+        membros.findByIdAndUpdate(id, req.body), (err) => {
+            if (err) {
+                res.status(500).send({ message: err.message })
+            } else {
+                res.status(200).send({ message: 'Membro atualizado com sucesso' })
+            }
+        }
+    }
+
+    static excluirLivro = (req, res) => {
+        const {id} = req.params;
+
+        livros.findByIdAndDelete(id, (err) => {
+            if (err) {
+                res.status(500).send({ message: err.message })
+            } else {
+                res.status(200).send({ message: 'Membro removido com sucesso' })
+            }
+        })
+    }
 
 }
 
