895553a01ce579514ea6c10a06da04740b851923
“MatGod135” <matheuslealcosta@icloud.com>
2022-12-15 19:04:30 -0300

    implementa routes de cargos

--- src/routes/cargosRoutes.js
+++ src/routes/cargosRoutes.js
@@ -4,3 +4,15 @@ import cargosController from "../controler/cargosController";
 const router = express.Router();
 
 router
+
+ .get('/cargos', cargosController.listarCargos)
+
+    .get("/cargos/:id", cargosController.getCargosById)
+
+    .post("/cargos", cargosController.cadastrarCargo)
+
+    .put("/cargos/:id", cargosController.atulizarCargo)
+
+    .delete("/cargos/:id", cargosController.excluirCargo)
+
+export default router;
